91c32f24f4ed4bf156c81193a8df4ba199a7e5c1
Jorgen Brandt <joergen.brandt@onlinehome.de>
2015-12-16 19:13:22 +0100

    Erlang updated to version 18.2.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,11 @@
+# coding: utf-8
 #
 # Cookbook Name:: chef-misc
 # Recipe:: default
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-# include_recipe "chef-misc::bitcoin"
-# include_recipe "chef-misc::erlang"
-# include_recipe "chef-misc::rebar"
+include_recipe "chef-misc::bitcoin"
+include_recipe "chef-misc::erlang"
+include_recipe "chef-misc::rebar"
 include_recipe "chef-misc::riak"
