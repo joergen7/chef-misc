254a5d40d568c0462b9f202e9fc63e593fbf2596
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-08-24 09:56:29 +0200

    Package dependencies added to cpntools.

--- recipes/cpntools.rb
+++ recipes/cpntools.rb
@@ -12,6 +12,12 @@ ct_dir = "#{node["dir"]["software"]}/CPNTools"
 directory node["dir"]["archive"]
 directory node["dir"]["software"]
 
+package "libxml2:i386"
+package "libglu1-mesa:i386"
+package "libgtk2.0-0:i386"
+package "libpangoxft-1.0-0:i386"
+package "libpangox-1.0-0:i386"
+
 remote_file ct_tar do
     action :create_if_missing
     source ct_link
