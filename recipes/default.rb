665cccfd106da775ec2131e03146d74624dacb03
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-12-19 10:17:04 +0100

    Erlang, Rebar3, and LFE updated.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,8 @@
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
 include_recipe "chef-misc::bitcoin"
-#include_recipe "chef-misc::erlang"
-#include_recipe "chef-misc::rebar3"
-#include_recipe "chef-misc::rebar"
-#include_recipe "chef-misc::lfe"
+include_recipe "chef-misc::erlang"
+include_recipe "chef-misc::rebar3"
+include_recipe "chef-misc::rebar"
+include_recipe "chef-misc::lfe"
 # include_recipe "chef-misc::hadoop"
--- recipes/erlang.rb
+++ recipes/erlang.rb
@@ -5,8 +5,8 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-erlang_vsn = "19.1"
-emulator_vsn = "8.1"
+erlang_vsn = "19.2"
+emulator_vsn = "8.2"
 erlang_link = "http://www.erlang.org/download/otp_src_#{erlang_vsn}.tar.gz"
 erlang_tar  = "#{node["dir"]["archive"]}/#{File.basename( erlang_link )}"
 erlang_dir  = "#{node["dir"]["software"]}/otp_src_#{erlang_vsn}"
